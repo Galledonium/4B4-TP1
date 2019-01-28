a274f281f214afbf8348b77882dd91e1a00060a8
Gregson Destin <gregsondestin@gmail.com>
2019-01-28 13:17:28 -0500

    Mise à terme de la création du constructeur avec les variables pertinents + Creation de deux objets CV

--- 4B4-TP1/src/CV.java
+++ 4B4-TP1/src/CV.java
@@ -1,5 +1,22 @@
 public class CV {
-		
+	
+	
+	CV gregson = new CV(
+			"Gregson",
+			"Destin",
+			"Informatique de Gestion",
+			"Centre d'appel Rotisserie St-Hubert (2 Novembre 2017 - ...)",
+			"Communication impeccable, Patience gargantuesque, Concentration accrue, Performant",
+			"Note finale minimale de 80%.");
+	CV isaac = new CV(
+			"Isaac David",
+			"Zolana",
+			"Informatique de Gestion",
+			"Centre d'appel en informatique à Revenu Quebec (13 Juin 2018 - ...)",
+			"Patient, Efficace, Performant, Attentif, Concentré, Sociable, Compatissant, Organisé, Respectueux",
+			"Passer le cours avec 100%."
+			);
+	
 	public String prenom;
 	public String nom;
 	public String formation;
@@ -7,6 +24,17 @@ public class CV {
 	public String competences;
 	public String attentes;
 	
+	public CV(String prenom, String nom, String formation, String expTravail, String competences, String attentes) {
+		
+		this.prenom = prenom;
+		this.nom = nom;
+		this.formation = formation;
+		this.expTravail = expTravail;
+		this.competences = competences;
+		this.attentes = attentes;
+		
+	}	
+
 	public static void main(String[] args) {
 		
 		System.out.println("Bienvenue chez Barette!");
@@ -15,8 +43,10 @@ public class CV {
 	
 	public void affiche(CV cv) {
 		
-
+		
 		
 	}
 	
+	
+
 }
