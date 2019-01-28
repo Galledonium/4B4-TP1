d5ca8164f59c5a635da0c5460528198ed73ac92a
Gregson Destin <gregsondestin@gmail.com>
2019-01-28 13:06:31 -0500

    Version initiale de la partie 1 du TP1. Inclut l'affichage du ttitre.

---
+++ 4B4-TP1/src/CV.java
@@ -0,0 +1,22 @@
+public class CV {
+		
+	public String prenom;
+	public String nom;
+	public String formation;
+	public String expTravail;
+	public String competences;
+	public String attentes;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Bienvenue chez Barette!");
+
+	}
+	
+	public void affiche(CV cv) {
+		
+
+		
+	}
+	
+}
