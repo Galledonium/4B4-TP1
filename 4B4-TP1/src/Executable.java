7b4645973c35324c9df4936adce1e100eed66f61
Gregson Destin <gregsondestin@gmail.com>
2019-02-18 13:05:17 -0500

    Ajout des classes: Fichier(Gestion de l'importation de fichier textes), Executable (Execute le programme); Ajout du fichier commandes.txt contenant les différentes commandes. La methode afficherFacture est maintenant intégrée a la classe Fichier au lieu de Commandes. La classe plat doit maintenant recevoir au minimum un repas afin d'etre initialisée.

---
+++ 4B4-TP1/src/Executable.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Executable {
+
+	
+	public static void main(String [] args) throws IOException {
+		
+		Fichier fichier = new Fichier ("C:\\Users\\Isaac David Zolana\\Desktop\\Workspace\\Assurance Qualité - TP1 - Partie 2\\src\\commandes.txt");
+
+		
+	}
+	
+}
