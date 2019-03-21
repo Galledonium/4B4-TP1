d3d67e93ee879657d3e7dffcaad743830083836d
Gregson Destin <gregsondestin@gmail.com>
2019-03-21 10:43:21 -0400

    Ajout du 2e test

--- 4B4-TP1-P3/src/test/FichierTest.java
+++ 4B4-TP1-P3/src/test/FichierTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import main.Client;
 import main.Fichier;
+import main.Plat;
 
 class FichierTest {
 
@@ -23,6 +24,21 @@ class FichierTest {
 		for(Client client : liste) {
 			
 			assertNotNull(client);
+			
+		}
+	}
+	
+	@Test
+	public void testAjoutClientDansListePlats() throws IOException {
+		Fichier fichier = new Fichier();
+		
+		fichier.lire();
+
+		ArrayList<Plat> liste = fichier.getListePlats();
+		
+		for(Plat plat : liste) {
+			
+			assertNotNull(plat);
 			
 		}
 	}
