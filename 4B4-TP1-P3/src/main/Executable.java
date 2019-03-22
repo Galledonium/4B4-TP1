63d8d85f5517f35e676545d8fcc0eb36f6626325
Isaac David Zolana <isaacworld2@hotmail.com>
2019-03-22 18:30:53 -0400

    Création des packages main et test + création du dossier fichiers + suppression de la classe CV.java

---
+++ 4B4-TP1-P3/src/main/Executable.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.io.IOException;
+
+public class Executable {
+	
+	public static void main(String [] args) throws IOException {
+		
+		Fichier fichier = new Fichier();
+
+		fichier.lecture();
+		fichier.ecrireFichier();
+	}
+	
+}
